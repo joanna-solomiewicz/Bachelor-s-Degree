7815b39f23a933454f63c67eb44509c07a1c56d6
Joanna Sołomiewicz <joanna.solomiewicz@student.put.poznan.pl>
2017-12-04 12:38:25 +0100

    Downloading esac

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -28,4 +28,34 @@ export class MainComponent implements OnInit {
       }
     )
   }
+
+  private downloadEsac(index: number): void {
+    const esac = this.esacs[index];
+    let link = document.createElement("a");
+    document.body.appendChild(link);
+    link.setAttribute('style', 'display: none');
+    const content = this.esacToString(esac);
+    const blob = new Blob([content], { type: 'text/plain' });
+    const url = window.URL.createObjectURL(blob);
+    const fileName = esac.name + '_' + esac.title + '.txt';
+
+    link.setAttribute('href', url);
+    link.setAttribute('download', fileName);
+    link.click();
+    window.URL.revokeObjectURL(url);
+  }
+
+  private esacToString(esac): string { //typ
+    let string = '';
+    string += esac.name + '\n';
+    string += 'CUT[' + esac.title + ']\n';
+    string += 'REG[' + esac.region + ']\n';
+    string += 'TRD[' + esac.source + ']\n';
+    string += 'SIG[' + esac.signature + ']\n';
+    string += 'KEY[' + esac.key + ']\n';
+    string += 'MEL[' + esac.melody + ']\n';
+    string += 'BEM[' + esac.remarks + ']';
+
+    return string;
+  }
 }
